d97a140ba17cb74cbf8c5c1acc97832b1faceb7a
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-15 00:00:51 +0300

    Polindrom

---
+++ Polindrom/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача полиндром
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int a1 = a / 10000;
+int a2 = a / 1000 % 10;
+int a3 = a / 10 % 10;
+int a4 = a % 10;
+if (a1 == a4 && a2 == a3)
+  Console.WriteLine("yes");
+else
+  Console.WriteLine("no");
