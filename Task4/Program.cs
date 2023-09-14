2d8e2d979809421508b58ac43dece47332fce13b
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-15 00:18:22 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число A
+// и выдает сумму чисел от 1 до A
+// 7->28
+// 4->10
+// 8->36
+double f(double x)
+{
+  return ((1 + x) / 2 * x);
+}
+
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write(f(a));
