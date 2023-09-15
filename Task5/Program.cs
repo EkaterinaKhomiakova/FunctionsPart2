83e0cf5fa84d4a6e3b6b1ba5781a14399d61ab66
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-15 17:23:41 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите прогамму, которая принимает на вход число
+// и выдает колличество цифр в числе
+// 456->3
+// 78->2
+// 89126->5
+int f(int a)
+{
+  if (a == 0)
+    return 1;
+  int count = 0;
+  while (a > 0)
+  {
+    a /= 10;
+    count++;
+  }
+  return count;
+}
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(a));
